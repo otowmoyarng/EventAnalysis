0bd9d0260f23a47ac22a377925ca131ca157e4c0
otowmoyarng <hidemusiam@gmail.com>
2022-09-25 12:13:31 +0900

    ファイル構成見直し

--- src/app.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
-
-app = Dash(__name__)
-
-if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', debug=True)
